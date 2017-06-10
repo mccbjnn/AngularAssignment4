e604788f1970ae1d11d5939964e8fe00a18cce9f
mccbjnn <mccbjnn@gmail.com>
2017-06-09 17:11:21 -0700

    MenuAppRestaurant Categories

---
+++ src/items.component.js
@@ -0,0 +1,22 @@
+  
+  (function () {
+  	'use strict';
+
+  	angular.module('MenuApp')
+  	.component('items', {
+  		templateUrl: 'src/templates/items.template.html',
+  		bindings: {
+
+  			items: '<',
+  		},
+
+
+  	});
+
+
+  })();
+
+
+
+
+  		
---
+++ src/menudata.service.js
@@ -0,0 +1,49 @@
+(function () {
+
+	'use strict';
+
+
+	angular.module('data')
+	.service('MenuDataService', MenuDataService)
+	.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+		
+	MenuDataService.$inject	= ['$http', 'ApiBasePath'];
+		function MenuDataService($http, ApiBasePath){
+
+			var service = this;
+
+			service.getAllCategories = function(){
+
+				var categories = $http({
+
+						method: "GET",
+
+						url: (ApiBasePath + "/categories.json")
+				}).then (function (response){
+
+					return response.data;
+				});
+					return categories;
+			};
+			
+			service.getItemsForCategory = function (categoryShortName) {
+
+				var data = $http({
+
+					method: "GET", 
+
+					url: (ApiBasePath + "/menu_items.json"),
+					
+					params: {
+						category: categoryShortName
+					}
+				}).then (function (response) {
+
+					return response.data;
+				});
+					return data;
+			};
+	
+		}
+	
+})();
